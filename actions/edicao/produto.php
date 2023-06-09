d42b87e01ba95e7baaae98a1e87037b08095bd8b
guto <joaoaugustolanjoni@gmail.com>
2023-06-09 01:09:07 -0300

    fix: inconsistência na edição de produtos
    # Inconsistência na tabela de produtos
    
    Edição agora verifica itens para evitar inconsistências no projeto de forma performática.

--- actions/edicao/produto.php
+++ actions/edicao/produto.php
@@ -4,13 +4,25 @@ $qtd_produto = $_SESSION['qtd_produto'];
 $id = $_POST['id_produto'];
 
 for ($i = 1; $i < $qtd_produto; $i++){
+    // Verificação de produtos existentes
+    if ($_SESSION['produto'][$i]['nome'] == $_POST['nome'] && 
+        $_SESSION['produto'][$i]['marca'] == $_POST['marca'] && 
+        $_SESSION['produto'][$i]['preco'] == $_POST['preco'] && 
+        $_SESSION['produto'][$i]['medida'] == $_POST['medida']) {
+        $_SESSION['produto_existe'] = true;
+        header("Location: /editar/produto?id=$id");
+        exit();
+    }
+
     if ($id == $_SESSION['produto'][$i]['id']){
-        $_SESSION['produto'][$i]['nome'] = $_POST['nome'];
-        $_SESSION['produto'][$i]['marca'] = $_POST['marca'];
-        $_SESSION['produto'][$i]['preco'] = $_POST['preco'];
-        $_SESSION['produto'][$i]['medida'] = $_POST['medida'];
+        $id_produto = $id;
     }
 }
+
+$_SESSION['produto'][$id_produto]['nome'] = $_POST['nome'];
+$_SESSION['produto'][$id_produto]['marca'] = $_POST['marca'];
+$_SESSION['produto'][$id_produto]['preco'] = $_POST['preco'];
+$_SESSION['produto'][$id_produto]['medida'] = $_POST['medida'];
 
 $_SESSION['produto_editado'] = true;
 header("Location: /produto");
